c75a141d6c49ac0bd030c4c5bbec788a0f610ccc
Rick429 <richaristico@gmail.com>
2022-03-21 17:48:40 +0100

    proyecto base spring

---
+++ blook/src/main/java/com/salesianostriana/blook/BlookApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.blook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlookApplication.class, args);
+	}
+
+}
---
+++ blook/src/test/java/com/salesianostriana/blook/BlookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.blook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
