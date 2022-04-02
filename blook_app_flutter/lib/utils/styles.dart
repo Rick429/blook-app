cc1917cac7153df87502593ffed938cc7164dc05
Rick429 <richaristico@gmail.com>
2022-04-02 14:58:48 +0200

    proyecto base

---
+++ blook_app_flutter/lib/utils/styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class BlookStyle {
+  // Colors
+  static const Color primaryColor = Color(0xFF8CC63E);
+  static const Color secondaryColor = Color(0xFF71973F);
+  static const Color tertiaryColor = Color(0xFF5B6F3F);
+  static const Color quaternaryColor = Color(0xFF404040);
+  static Color greyBoxColor = const Color(0xFF6C727F).withOpacity(40);   
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+
+  // Margins, Paddings, Card properties
+  static const double bodyPadding = 20.0;
+  static const double cardElevation = 1.0;
+  static const double cardBorderRadius = 15;
+
+  // Font sizes, TextStyles
+  static const double textSizeZero = 10.0;
+  static const double textSizeOne = 12.0;
+  static const double textSizeTwo = 14.0;
+  static const double textSizeThree = 18.0;
+  static const double textSizeFour = 20.0;
+  static const double textSizeFive = 24.0;
+  static const double textSizeSix = 64.0;
+
+  static TextStyle get textTitle => GoogleFonts.getFont(
+        'Mohave',
+        color: BlookStyle.whiteColor,
+        fontWeight: FontWeight.w600,
+        fontSize: 35,
+      );
+
+  static TextStyle textTitleCustom(Color c, double fSize) => GoogleFonts.getFont(
+        'Nunito',
+        color: c,
+        fontWeight: FontWeight.w700,
+        fontSize: fSize,
+      );
+}
---
+++ blook_app_flutter/lib/widgets/home_app_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 70),
+      child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Expanded(
+          flex: 0,
+          child: SvgPicture.asset('assets/images/logo.svg', width: 100,
+          ),
+        ),
+        Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                      Navigator.pushNamed(context, '/search');
+                    },
+                  child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.search),
+                  ),
+                ),
+                 GestureDetector(
+                  onTap: () {
+                      Navigator.pushNamed(context, '/add');
+                    },
+                  child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(Icons.add),
+                  ),
+                ),
+              ],
+            ))
+      ]),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+}
