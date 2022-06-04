e15edaa9e02bf6f41ac25506d9668e4032c9b4d5
Rick429 <richaristico@gmail.com>
2022-06-04 22:28:32 +0200

    cambio edit

--- blook/src/main/java/com/salesianostriana/blook/dtos/EditUserDto.java
+++ blook/src/main/java/com/salesianostriana/blook/dtos/EditUserDto.java
@@ -3,6 +3,7 @@ package com.salesianostriana.blook.dtos;
 import com.salesianostriana.blook.validation.anotations.EditEmail;
 import lombok.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +18,7 @@ public class EditUserDto {
     private String nick;
     private String name;
     private String lastname;
+    @NotBlank(message = "{user.email.blank}")
     @Email(message = "{user.email.email}")
     private String email;
 }
