cf364bbda022dfcbcfa348b3b6134dffa31e84ac
Rick429 <richaristico@gmail.com>
2022-06-13 15:28:02 +0200

    cambio environment

--- blook-angular/src/environments/environment.prod.ts
+++ blook-angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  /* API_BASE_URL: 'https://api-blook.herokuapp.com/', */
-  API_BASE_URL: 'http://localhost:8080/',
+  API_BASE_URL: 'https://api-blook.herokuapp.com/',
+  /* API_BASE_URL: 'http://localhost:8080/', */
 };
